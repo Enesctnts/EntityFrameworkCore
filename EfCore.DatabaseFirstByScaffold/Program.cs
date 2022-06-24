98766bb8e4c7d1875c24dc50f97323b29e5a923d
Enes Çetintaş <enesctnts@gmail.com>
2022-06-24 22:47:24 +0300

    EntityFrameworkCore Scaffold yöntemi sayesinde veri tabanındaki tabloları otomatik olarak c# üzerinde kendisi oluşturuyor. Bu işlemi başarılı bir şekilde yapıldı. Ürün listeleme işlemi yapıldı.

--- EfCore.DatabaseFirstByScaffold/Program.cs
+++ EfCore.DatabaseFirstByScaffold/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EfCore.DatabaseFirstByScaffold.Models;
+using Microsoft.EntityFrameworkCore;
 
+using (var context = new EfCoreDatabaseFirstContext())
+{
+    var products = await context.Products.ToListAsync();
+    products.ForEach(p =>
+    Console.WriteLine($"{p.Id} {p.Name} {p.Price}")
+    );
+}
+
+
+//Bu yöntem Scaffold yöntemi deniyor. Veri tabanındaki tabloları otomatik olarak c# üzerinde kendisi oluşturuyor. 
+//Scaffold-DbContext "Data Source=DESKTOP-B83TJ29;Initial Catalog=EfCoreDatabaseFirst;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
