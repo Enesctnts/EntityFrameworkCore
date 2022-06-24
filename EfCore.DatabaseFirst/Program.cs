a30a96638a40a944a45b5a1af1276dd3c3fc1a11
Enes Çetintaş <enesctnts@gmail.com>
2022-06-24 23:17:01 +0300

    Veri tabanından ürün listeleme işlemi için DatabaseFirst yaklamı kullanıldı.

---
+++ EfCore.DatabaseFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EfCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+DbContexInitializer.Build();
+using (var context = new AppDbContext(DbContexInitializer.OptionsBuilder.Options))
+{
+    var products = await context.Products.ToListAsync();
+    products.ForEach(p =>
+    Console.WriteLine($"{p.Name} {p.Price}")
+    );
+}
