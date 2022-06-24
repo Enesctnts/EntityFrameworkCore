6c6d7b86b0ed4acdd28c5cbfa2455d7fead7c1ff
Enes Çetintaş <enesctnts@gmail.com>
2022-06-24 23:14:33 +0300

    //IConfigurationRoot appsetting okumamızı sağlar. DbContextOptionsBuilder constructurda options verebilmemiz için oluşturuldu.

---
+++ EfCore.DatabaseFirst/DAL/DbContexInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.DatabaseFirst.DAL
+{
+    public class DbContexInitializer
+    {
+        //IConfigurationRoot appsetting okumamızı sağlar
+        public static IConfigurationRoot Configurations;
+        public static DbContextOptionsBuilder<AppDbContext> OptionsBuilder;
+
+        public static void Build()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configurations = builder.Build();
+            OptionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            OptionsBuilder.UseSqlServer(Configurations.GetConnectionString("SqlCon"));
+
+        }
+
+
+    }
+}
