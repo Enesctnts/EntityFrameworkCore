51e08881eb3c4483a17f297dd886a2fa7ef61f54
Enes Çetintaş <enesctnts@gmail.com>
2022-06-25 00:21:43 +0300

    Code first işlemi yapıldı. Veriler listelendi

---
+++ EFCore.CodeFirst/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore.CodeFirst;
+using EFCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+using (var context = new AppDbContext())
+{
+    var products = await context.Products.ToListAsync();
+    products.ForEach(p =>
+    Console.WriteLine($"{p.Name} {p.Price}")
+    );
+}
