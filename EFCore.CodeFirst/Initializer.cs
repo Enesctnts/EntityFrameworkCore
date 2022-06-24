3ecac0c70ea103db032f363dfe3256d2a095d493
Enes Çetintaş <enesctnts@gmail.com>
2022-06-25 00:21:23 +0300

    Appsettings'ten dinamik olarak ConnectionString okuyabilmek için Initializer classı oluşturuldu.

---
+++ EFCore.CodeFirst/Initializer.cs
@@ -0,0 +1,24 @@
+﻿using EFCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.CodeFirst
+{
+    public class Initializer
+    {
+        public static IConfigurationRoot Configurations;
+
+        public static void Build()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configurations = builder.Build();
+
+        }
+    }
+}
