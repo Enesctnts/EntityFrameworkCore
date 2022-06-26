c34c7ee41eed7ffdb6dc217e1437c19a23dd61ea
Enes Çetintaş <enesctnts@gmail.com>
2022-06-26 22:34:59 +0300

    AsNoTracking() ve ChangeTracker' ı kullanıldı. Bunlar hakkında bilgiler not edildi.

--- EFCore.CodeFirst/DAL/AppDbContext.cs
+++ EFCore.CodeFirst/DAL/AppDbContext.cs
@@ -26,5 +26,31 @@ namespace EFCore.CodeFirst.DAL
             Initializer.Build();
             optionsBuilder.UseSqlServer(Initializer.Configurations.GetConnectionString("SqlCon"));
         }
+
+        public override int SaveChanges()
+        {
+            //EntityFramework listeleme,ekleme,güncelleme,silme işlemlerini önce Tracker'a alır.Burda add,update,delete,list şeklinde işlemlerin durumu(State) ne ise o şekilde tutulur. SaveChanges yapıldıktan sonra veritabanına bildirilir.Bildirilmeden önce burda tutuğu için Ram'de yer kaplar ve sistemi yorar. Örnek verirsek 1000 tane ürün listelediğimiz zaman bu ürünlerin hepsini var products= dbcontext.product.tolist() yapıldında tracker'a alır. Burada ekleme silme veya güncelleme yapılmadıgı için trackerda yer kaplamasına yani ram' i yormasına gerek yok.
+            //Bu durumlar için var products = Products.AsNoTracking().ToList(); yaparak Tracker'a alma deriz.Sistemi yormamış oluruz.
+
+            //ChangeTracker propertysi sayesinde Tracker alınmış ürünleri görebiliyoruz.Ekleme,silme,güncelleme yapıldığı zaman Track edilmesini istiyoruz. Bu işlemler yapılacagı zaman track edilecek ürünler. Sonuçta ürün yeni eklendiği zaman track edilceği için track edilmiş mi yani soruyoruz edildiyse ürün yeni ekleniyor.
+            
+            //Burda Track edilen değerler yeni ürünler olduğu için CreatedDate ni merkezi yerden değerini vermiş oluyoruz. Otomatik olarak şuan ki tarihi atmış oluyoruz. Bu SAVECHANGES Metodunu ezmemizin sebebi SaveChanges yaptığımız zaman önce bu işlem yapılacak sonra veri tabanına kaydedicek.
+            ChangeTracker.Entries().ToList().ForEach(e =>
+            {
+                //if'in içerisinde track edilmiş değerlerden entitysi Product'a dönüştürebiliyorsa dönüştürür ve p' ye at diyoruz. bu işlemi is keywordü sayesinde yapabiliyoruz. is keywordu dönüştürme işleminde true ya da false değeri döndürür true ise if'in içine girer.  
+                if (e.Entity is Product p)
+                {
+                    //Track edilmiş ürünlerin o anki durumu ekleme ise if'e girer. Yeni oluşturulduğu için createdDate otomatik şu anki tarihi atar.
+                    if (e.State == EntityState.Added)
+                    {
+                        p.CreatedDate = DateTime.Now;
+                    }
+                }
+                
+            });
+                
+            return base.SaveChanges();
+        }
+
     }
 }
