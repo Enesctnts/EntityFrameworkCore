7041c3faa0d6236d50e2256ed06f409ffb078a47
Enes Çetintaş <enesctnts@gmail.com>
2022-06-28 18:42:19 +0300

    Category işlemleri yapıldı.

---
+++ EfCore.RelationShip.OneToMany/Entity/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.RelationShip.OneToMany.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+} 
