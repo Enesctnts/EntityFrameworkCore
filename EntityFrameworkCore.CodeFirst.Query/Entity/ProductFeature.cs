47c20d95d545f87234f9187262db2080a1a394fb
Enes Çetintaş <enesctnts@gmail.com>
2022-06-29 02:38:00 +0300

    ProductFeature propertyleri oluşturuldu.

---
+++ EntityFrameworkCore.CodeFirst.Query/Entity/ProductFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.CodeFirst.Query.Entity
+{
+    public class ProductFeature
+    {
+        [ForeignKey("Product")]
+        public int Id { get; set; } 
+        public int Width { get; set; }
+        public int Height { get; set; } 
+        public string? Color { get; set; }
+        public Product Product { get; set; }
+    }
+}
