78c894d5331d8c56c4b0212d18edde22dbd87646
Enes Çetintaş <enesctnts@gmail.com>
2022-06-29 02:37:40 +0300

    Product propertyleri oluşturuldu.

---
+++ EntityFrameworkCore.CodeFirst.Query/Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.CodeFirst.Query.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Barcode { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = new();
+        public ProductFeature ProductFeature { get; set; } = new(); 
+    }
+}
